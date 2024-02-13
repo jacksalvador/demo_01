88f7d53c25a7785c08e20953dbc813c9ea76cf0e
yangiksoon <iksoon.yang@lgcns.com>
2024-02-13 18:12:44 +0900

    tag spello fix

--- cicd/terraform/ddb.tf
+++ cicd/terraform/ddb.tf
@@ -22,14 +22,7 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
     enabled        = false
   }
 
-  tags = {
-    Name        = "dynamodb-table-1"
-    Environment = "production"
-  }
-
   tags = merge(var.common_tags, {
-    Name     = "ec2-${var.aws_region}-${var.environment}-board"
-    "Backup" = "False"
-    "Ssm"    = "datascientists"
+    Name = "tag test"
   })
 }
